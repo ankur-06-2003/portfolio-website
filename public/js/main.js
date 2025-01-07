23ed185498c0ce5129daaa6da8c50e6c3f47da64
ankur <ankur@ankurs-MacBook-Air.local>
2025-01-07 13:13:22 +0530

    change header image

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,45 @@
 
 function displayBar(){
-    document.querySelector("#bar").style.display="none";
-    document.querySelector("#xcross").style.display='block';
-    document.querySelector(".container nav ul").style.display='block';
+    
+    document.querySelector("#sidebar").style.display='block';
+    
   
 }
 function CloseBar(){
-    document.querySelector("#xcross").style.display="none";
-    document.querySelector("#bar").style.display="block";
-    document.querySelector(".container nav ul").style.display='none';
+    document.querySelector("#sidebar").style.display="none";
+    
+    
 }
 function mySkill(){
     document.querySelector("#tab-contents-edu").style.display="none";
     let skl=document.querySelector(".tab-contents");
     skl.style.display="block"
     document.querySelector("#my-edu").setAttribute("class","tab-links")
-    document.querySelector("#my-skill").setAttribute("class","tab-links  active-link")
+    document.querySelector("#my-skill").classList.add("active-link")
 }
+// function bodyscroll(){
+//     if(window.innerHeight(1000)){
+//         document.querySelector(".css-progress").style.width="80%";
+//     // .progress:hover .css-progress{
+//     let a= document.querySelectorAll(".html-progress");
+//     a.forEach((curr)=>{
+//         curr.style.width='90%'
+//     })}
+//     //     width: 80%;
+//     // }
+//     // .progress:hover .api-progress{
+//     //     width: 70%;
+//     // }
+//     // .progress:hover .html-progress{
+//     //     width: 90%;
+//     }
+
 function edu(){
     document.querySelector(".tab-contents").style.display="none";
     let skl=document.querySelector("#tab-contents-edu");
     skl.style.display="block"
     document.querySelector("#my-skill").setAttribute("class","tab-links")
-    document.querySelector("#my-edu").setAttribute("class","tab-links  active-link")
+    document.querySelector("#my-edu").classList.add("active-link")
 }
 function about(){
     if(window.innerWidth<=800){
